f6ca9b558ce6895e8bb3b833740e437e3c0b478c
Eugene Hong <awidesky@naver.com>
2026-05-04 21:40:57 +0900

    include glew.h if GDM_HAS_GLEW

--- external/glutil/include/glutil/gl.hpp
+++ external/glutil/include/glutil/gl.hpp
@@ -1,7 +1,11 @@
 #ifndef GLUTIL_GL_HPP
 #define GLUTIL_GL_HPP
 
-#ifdef GDM_HAS_GLAD
+#if defined(GDM_HAS_GLEW)
+// In glew-glad mode, <GL/glew.h> is a generated shim that pulls in both
+// the GLEW-facing API and GLAD-backed function mapping.
+#include <GL/glew.h>
+#elif defined(GDM_HAS_GLAD)
 #include <glad/gl.h>
 #endif
 
