099a93e1e18f102c17820b9e49ee494caed4d9de
SSW20 <anfdmlrltk11@naver.com>
2026-05-18 14:24:11 +0900

    Edit snapshot example, call snapshot twice

--- external/glutil/example/snapshot.cpp
+++ external/glutil/example/snapshot.cpp
@@ -145,7 +145,10 @@ int main()
     glUniform1i(glGetUniformLocation(shader, "tex"), 0);
 
     //glutil::debug::snapshot();
-    glutil::debug::snapshot{}.bufferVAOInfo(true,true,true,true).capture(std::cerr);
+    //glutil::debug::snapshot ss = glutil::debug::snapshot{}.bufferVAOInfo(true, true, true, true);
+    glutil::debug::snapshot ss = glutil::debug::snapshot{}.bufferVAOInfo(true, true, true, true).printPerCall(true);
+    ss.capture();
+    ss.capture();
     while (!glfwWindowShouldClose(window)) {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, true);
