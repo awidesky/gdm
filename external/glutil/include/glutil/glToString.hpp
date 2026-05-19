0963778609b9be0066a4eaa7cd7d49de14bf043b
Eugene Hong <40064726+awidesky@users.noreply.github.com>
2026-05-19 22:22:23 +0900

    Update glToString.hpp to use size_t

--- external/glutil/include/glutil/glToString.hpp
+++ external/glutil/include/glutil/glToString.hpp
@@ -1,7 +1,8 @@
-﻿#ifndef GLUTIL_GLTOSTRING_HPP
+#ifndef GLUTIL_GLTOSTRING_HPP
 #define GLUTIL_GLTOSTRING_HPP
 
 #include <glutil/gl.hpp>
+#include <cstddef>
 
 namespace glutil {
 
@@ -15,6 +16,20 @@ constexpr inline const char* glTypeToString(GLint type) {
         case GL_SHORT: return "GL_SHORT";
         case GL_BYTE: return "GL_BYTE";
         default: return "UNKNOWN";
+    }
+}
+
+constexpr inline std::size_t glTypeSize(GLenum type) {
+    switch (type) {
+        case GL_FLOAT: return sizeof(GLfloat);
+        case GL_DOUBLE: return sizeof(GLdouble);
+        case GL_INT: return sizeof(GLint);
+        case GL_UNSIGNED_INT: return sizeof(GLuint);
+        case GL_SHORT: return sizeof(GLshort);
+        case GL_UNSIGNED_SHORT: return sizeof(GLushort);
+        case GL_BYTE: return sizeof(GLbyte);
+        case GL_UNSIGNED_BYTE: return sizeof(GLubyte);
+        default: return sizeof(unsigned char);
     }
 }
 
@@ -131,20 +146,6 @@ constexpr inline const char* glBufferTypeToString(GLenum target) {
         case GL_TRANSFORM_FEEDBACK_BUFFER: return "GL_TRANSFORM_FEEDBACK_BUFFER";
         case GL_UNIFORM_BUFFER: return "GL_UNIFORM_BUFFER";
         default: return "UNKNOWN_BUFFER";
-    }
-}
-
-constexpr inline int glTypeSize(GLenum type) {
-    switch (type) {
-        case GL_FLOAT: return sizeof(GLfloat);
-        case GL_DOUBLE: return sizeof(GLdouble);
-        case GL_INT: return sizeof(GLint);
-        case GL_UNSIGNED_INT: return sizeof(GLuint);
-        case GL_SHORT: return sizeof(GLshort);
-        case GL_UNSIGNED_SHORT: return sizeof(GLushort);
-        case GL_BYTE: return sizeof(GLbyte);
-        case GL_UNSIGNED_BYTE: return sizeof(GLubyte);
-        default: return sizeof(unsigned char);
     }
 }
 
