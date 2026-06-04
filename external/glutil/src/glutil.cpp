4f497ee9ce0b2e4e97ba6d2c49b0f65e2a2c4adf
Eugene Hong <awidesky@naver.com>
2026-06-04 11:25:45 +0900

    empty glutil.cpp

--- external/glutil/src/glutil.cpp
+++ external/glutil/src/glutil.cpp
@@ -1,17 +1,1 @@
-#include <glutil/glutil.hpp>
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
-#include <vector>
-
-namespace glutil {
-
-} // namespace glutil
+namespace glutil {} // namespace glutil
